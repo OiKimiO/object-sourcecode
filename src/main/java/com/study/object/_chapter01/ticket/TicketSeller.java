835c8d784ecb70d23e5fedabbe70eb3e28f2938c
OiKimiO <etkim02@naver.com>
2023-07-02 13:32:04 +0900

    Add #9 - 티켓 판매자를 만들어라
    - 티켓 판매소(TicketOffice)가 있습니다.
    - 누구든 티켓 판매소에 접근해요: getTicketOffice

---
+++ src/main/java/com/study/object/_chapter01/ticket/TicketSeller.java
@@ -0,0 +1,9 @@
+package com.study.object._chapter01.ticket;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketOffice getTicketOffice(){
+        return ticketOffice;
+    }
+}
