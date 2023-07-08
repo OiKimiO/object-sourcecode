68af8beed7a5b1e90e8fd3ef8defbb9c82fd1089
OiKimiO <etkim02@naver.com>
2023-07-08 23:34:01 +0900

    Add #25 - 공통 할인 정책 세부 구현 > 정률 할인 정책을 만들어라
    - 구현 = 할인 금액(Money)이 있습니다.
    - 생성자 = 할인 금액, 할인 조건(부모)을 입력 받습니다.
    - 공용 = 상영에 대한 할인 금액을 가져오세요: Money getDiscountAmount(Screening)
    	- 구현 = 할인금액을 가져오세요.

---
+++ src/main/java/com/study/object/chapter02/Movie/AmountDiscountPolicy.java
@@ -0,0 +1,15 @@
+package com.study.object.chapter02.Movie;
+
+public class AmountDiscountPolicy extends DiscountPolicy{
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    public Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
+}
