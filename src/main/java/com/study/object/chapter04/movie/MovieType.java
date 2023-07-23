8d15073fb9c3637a1157d784612c40da4d6018c5
OiKimiO <etkim02@naver.com>
2023-07-23 17:41:14 +0900

    Add #34 - 영화 할인 구분을 만들어라
    - 고정 할인 정책(AMOUNT_DISCOUNT), 비율 할인 정책(PERCENT_DISCOUNT), 미적용(NONE_DISCOUNT)가 있습니다.

---
+++ src/main/java/com/study/object/chapter04/movie/MovieType.java
@@ -0,0 +1,9 @@
+package com.study.object.chapter04.movie;
+
+import java.time.Duration;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT,  // 고정 할인 정책
+    PERCENT_DISCOUNT, // 비율 할인 정책
+    NONE_DISCOUNT;    // 미적용
+}
