8479329f58c522f43c13c80f4ecf3da0fecd9255
OiKimiO <etkim02@naver.com>
2023-07-23 18:06:20 +0900

    Add #39 - 관람객을 만들어라
    - 구현 = 관람객명(String)과 관람객 id(String)가 있습니다.
    - 생성자 = 관람객은 관람객명과 관람객 id를 입력합니다.

---
+++ src/main/java/com/study/object/chapter04/movie/Audience.java
@@ -0,0 +1,11 @@
+package com.study.object.chapter04.movie;
+
+public class Audience {
+    private String name;
+    private String id;
+
+    public Audience(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
