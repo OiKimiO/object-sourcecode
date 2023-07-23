00245ce3534efb9f3d06b7d8ca30716bae7b82bc
OiKimiO <etkim02@naver.com>
2023-07-23 17:44:51 +0900

    Add #35 - 할인 조건 구분을 만들어라
    - 순번 조건(SEQUENCE), 기간 조건(PERIOD)이 있습니다.
    - 영화 타입 구분 세미콜론을 제거했습니다.

---
+++ src/main/java/com/study/object/chapter04/movie/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package com.study.object.chapter04.movie;
+
+public enum DiscountConditionType {
+    SEQUENCE,   // 순번 조건
+    PERIOD      // 기간 조건
+}
--- src/main/java/com/study/object/chapter04/movie/MovieType.java
+++ src/main/java/com/study/object/chapter04/movie/MovieType.java
@@ -5,5 +5,5 @@ import java.time.Duration;
 public enum MovieType {
     AMOUNT_DISCOUNT,  // 고정 할인 정책
     PERCENT_DISCOUNT, // 비율 할인 정책
-    NONE_DISCOUNT;    // 미적용
+    NONE_DISCOUNT    // 미적용
 }
