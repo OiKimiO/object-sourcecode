3b85e982132d39e5c71d1bdb454e2f8d12ec2496
OiKimiO <etkim02@naver.com>
2023-07-23 18:00:20 +0900

    Add #37 - 영화를 만들어라
    - 구현 = 제목(String), 상영시간(Duration), 관람료(Money), 할인조건들(`List<DiscountCondition>`), 영화 타입(MovieType), 할인 금액(Money), 할인 비율(Money)이 있습니다.
    - 공용 = 영화 타입, 관람료, 할인 조건들, 할인 금액, 할인 비율에 대한 getter/setter를 만듭니다.
    - 할인 조건들의 경우 리턴하는 List가 수정되지 않게 만들어 주세요

---
+++ src/main/java/com/study/object/chapter04/movie/Movie.java
@@ -0,0 +1,57 @@
+package com.study.object.chapter04.movie;
+
+import com.study.object.chapter02.movie.Money;
+
+import java.time.Duration;
+import java.util.*;
+
+public class Movie {
+    private String title;
+    private Duration duration;
+    private Money fee;
+    private List<DiscountCondition> discountConditions = new ArrayList<>();
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
