d4b9fe8236cd7626b047903997025c5dd3eeb1b4
OiKimiO <etkim02@naver.com>
2023-07-25 00:25:43 +0900

    Add #45 - 개선: 영화를 자율적인 존재로 만들어라
    - 공용 = 영화 조건 구분을 가져오세요
    - 공용 = 고정 할인 금액 관람료를 계산하세요: Money calculateAmountDiscountedFee()
    	- 구현 = 고정 할인 정책이 아닌 경우 exception처리하고, 맞다면 할인된 관람료를 계산하세요
    - 공용 = 비율 할인 금액 관람료를 계산하세요: Money calculatePercentDiscountedFee()
    	- 구현 = 비율 할인 정책이 아닌 경우 exception처리하고, 맞다면 할인된 관람료를 계산하세요
    - 공용 = 할인이 없는 금액 관람료를 계산하세요: Money calculateNoneDiscountedFee()
    	- 구현 = 할인이 없는 정책이 아닌 경우 exception처리하고, 맞다면 관람료를 그대로 가져오세요
    - 공용 = 이 상영 시간과 순번으로 할인받을 수 있는지 확인하세요: boolean isDiscountable(LocalDateTime, int)
    	- 구현 = 할인 조건을 하나씩 비교하여 기간 조건인지 순번 조건인지 확인합니다.
    - 예약 대행사(ReservationAgency)의 discountable을 movie의 isDiscountable로 처리하고 이후 관람료를 위의 메서드로 처리합니다.

--- src/main/java/com/study/object/chapter04/movie/ReservationAgency.java
+++ src/main/java/com/study/object/chapter04/movie/ReservationAgency.java
@@ -5,31 +5,13 @@ import com.study.object.chapter02.movie.Money;
 public class ReservationAgency {
     public Reservation reserve(Screening screening, Customer customer, int audienceCount){
         Movie movie = screening.getMovie();
-        boolean discountable = false;
-        for (DiscountCondition condition : movie.getDiscountConditions()) {
-            discountable = switch(condition.getType()){
-                                case PERIOD -> condition.isDiscountable(screening.getWhenScreened().getDayOfWeek(),
-                                                                        screening.getWhenScreened().toLocalTime());
-                                case SEQUENCE -> condition.isDiscountable(screening.getSequence());
-                            };
+        boolean discountable = movie.isDiscountable(screening.getWhenScreened(), screening.getSequence());
 
-            if(discountable){
-                break;
-            }
-        }
-
-        Money fee;
-        if(discountable){
-            Money discountAmount = switch (movie.getMovieType()){
-                                        case AMOUNT_DISCOUNT  -> movie.getDiscountAmount();
-                                        case PERCENT_DISCOUNT -> movie.getFee().times(movie.getDiscountPercent());
-                                        default -> Money.ZERO;
-                                    };
-
-            fee = movie.getFee().minus(discountAmount);
-        }else{
-            fee = movie.getFee();
-        }
+        Money fee = switch (movie.getMovieType()){
+                        case AMOUNT_DISCOUNT  -> movie.calculateAmountDiscountedFee();
+                        case PERCENT_DISCOUNT -> movie.calculatePercentDiscountedFee();
+                        case NONE_DISCOUNT -> movie.calculateNoneDiscountedFee();
+                    };
 
         return new Reservation(customer, screening, fee, audienceCount);
     }
